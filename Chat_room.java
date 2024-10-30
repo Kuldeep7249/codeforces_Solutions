80d75defecfb908920a03de23d677bc4ca3075bd
Kuldeep7249 <152018228+Kuldeep7249@users.noreply.github.com>
2024-10-30 23:11:32 +0530

    question 58/a added

--- Chat_room.java
+++ Chat_room.java
@@ -1,13 +1,23 @@
-package Codeforces;
 import java.util.*;
+
 public class Chat_room {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s  = sc.nextLine();
-        int i = 0;
-        String d = "hello";
-        while(i < s.length()){
-
+        String s = sc.nextLine();
+        StringBuilder a = new StringBuilder("hello");
+        for (char b : s.toCharArray()) {
+            if (a.length() == 0) {
+                break;
+            }
+            if (b == a.charAt(0)) {
+                a.deleteCharAt(0);
+            }
+        }
+        if(a.length()==0){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
         }
     }
 }
