046ed4d420700a5640ebf7225f5a3600757a7c88
Kuldeep7249 <152018228+Kuldeep7249@users.noreply.github.com>
2024-10-30 22:50:43 +0530

    some question solution added into the repo

---
+++ Chat_room.java
@@ -0,0 +1,13 @@
+package Codeforces;
+import java.util.*;
+public class Chat_room {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s  = sc.nextLine();
+        int i = 0;
+        String d = "hello";
+        while(i < s.length()){
+
+        }
+    }
+}
